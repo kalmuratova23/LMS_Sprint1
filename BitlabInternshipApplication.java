e2929b63f2089e0c81dd137508670292dc62ba12
kalmuratova23 <yerlankyzyyy@gmail.com>
2024-11-19 16:16:11 +0500

    Add files via upload

---
+++ BitlabInternshipApplication.java
@@ -0,0 +1,13 @@
+package kz.pryahin.bitlabInternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitlabInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitlabInternshipApplication.class, args);
+	}
+
+}
